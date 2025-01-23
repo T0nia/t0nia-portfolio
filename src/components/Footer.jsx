5cf87875a97252aba0e4f978f1c6738203579732
T0nia <edtoniaelohor@gmail.com>
2025-01-23 21:46:30 +0100

    prevent defaul submission behavior

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
+  const handleSubscribe = (e) => {
+    e.preventDefault(); 
+    // my subscription logic will be here when I am ready
+  
+  };
+
   return (
     <footer className="bg-black text-white py-8">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <div className="flex flex-col md:flex-row md:space-x-12 items-center mb-4">
           <div className="flex-1 mb-4 md:mb-0">
-            <h3 className="text-2xl font-bold mb-2">T0nia</h3>
+            <h3 className="text-2xl font-bold mb-2">Tonia</h3>
             <p className="text-gray-400">
               Frontend Engineer, specializing in the development of modern and responsive web applications.
             </p>
           </div>
           <div className="flex-1 w-full">
-            <form className="flex items-center justify-center">
+            <form onSubmit={handleSubscribe} className="flex items-center justify-center">
               <input
                 type="email"
                 placeholder="Your email"
@@ -21,7 +27,7 @@ const Footer = () => {
                 focus:outline-none focus:border-green-400"
               />
               <button
-                type="submit"
+                type="submit" 
                 className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 
                 py-2 rounded-r-lg"
               >
